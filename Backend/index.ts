3d11bfa15ae79430d81d81cdb7217004780c480a
Cristian C. <cristiancandidatu@hotmail.com>
2025-09-01 15:20:43 +0300

    init

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express"; // ← tip-only import
 
 const app = express();
 const port = 3000;
